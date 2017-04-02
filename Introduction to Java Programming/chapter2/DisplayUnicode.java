b3d7a1c7e728279011d155df4a706361e08713b6
7-sevens <sevens2008@163.com>
2017-04-03 01:26:15 +0800

    practice of chapter2

---
+++ Introduction to Java Programming/chapter2/DisplayUnicode.java
@@ -0,0 +1,14 @@
+package chapter2;
+
+import javax.swing.JOptionPane;
+
+public class DisplayUnicode {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "\u6B22\u8FCE \u03b1 \u03b2 \u03b3," +
+				"\u6B22\u8FCE Welcome");
+	}
+}
---
+++ Introduction to Java Programming/chapter2/FahrenheitToCelsius.java
@@ -0,0 +1,20 @@
+package chapter2;
+
+import java.util.Scanner;
+
+public class FahrenheitToCelsius {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter a degree in Fahrenheit: ");
+		double fahrenheit = input.nextDouble();
+		
+		// Convert Fahrenheit to Celsius
+		double celsius = (0.5 / 9) * (fahrenheit - 32);
+		System.out.println("Fahrenheit " + fahrenheit + " is " + celsius + " in Celsius");
+	}
+}
