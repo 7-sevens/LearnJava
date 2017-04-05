3a393c200ba1e0baf3b89d27139accdd1e9e9a42
7-sevens <sevens2008@163.com>
2017-04-05 19:30:14 +0800

    chapter5 practice

---
+++ Introduction to Java Programming/chapter5/TestContinue.java
@@ -0,0 +1,22 @@
+package chapter5;
+
+public class TestContinue {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int sum = 0;
+		int number = 0;
+		
+		while (number < 20) {
+			number++;
+			if (number == 10 || number == 11)
+				continue;
+			sum += number;
+		}
+		
+		System.out.println("The sum is " + sum);
+	}
+
+}
