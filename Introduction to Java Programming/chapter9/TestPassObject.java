4424c77528a1bfbbe1fa41575cdbad5353998ff9
7-sevens <sevens2008@163.com>
2017-04-07 13:58:58 +0800

    chapter9 practice

---
+++ Introduction to Java Programming/chapter9/TestPassObject.java
@@ -0,0 +1,24 @@
+package chapter9;
+
+public class TestPassObject {
+
+	/** Main method */
+	public static void main(String[] args) {
+		// Create a Circle object with radius 1
+		CircleWithPrivateDataFields myCircle = new CircleWithPrivateDataFields(1);
+		
+		// Print areas for radius 1, 2, 3, 4, and 5.
+		int n = 5;
+		printAreas(myCircle, n);
+	}
+
+	/** Print a table of areas for radius */
+	public static void printAreas(CircleWithPrivateDataFields c, int times) {
+		System.out.println("Radius \t\tAres");
+		while (times >= 1) {
+			System.out.println(c.getRadius() + "\t\t" + c.getArea());
+			c.setRadius(c.getRadius() + 1);
+			times--;
+		}
+	}
+}
